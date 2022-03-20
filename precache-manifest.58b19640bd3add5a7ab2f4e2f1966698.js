bb05e312e2bb30b25ac95795ef7515dc8b013f9d
Dima Kolyas <dimakol@gmail.com>
2022-03-20 21:50:53 +0200

    Updates

---
+++ precache-manifest.58b19640bd3add5a7ab2f4e2f1966698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0238d290e32e9526a59f7ddc2c4256",
+    "url": "/react-import-export-excel/index.html"
+  },
+  {
+    "revision": "793eefb96e4c7cbaf35a",
+    "url": "/react-import-export-excel/static/css/main.db58e7a0.chunk.css"
+  },
+  {
+    "revision": "c57a1fae7008182a8632",
+    "url": "/react-import-export-excel/static/js/2.342a262c.chunk.js"
+  },
+  {
+    "revision": "793eefb96e4c7cbaf35a",
+    "url": "/react-import-export-excel/static/js/main.dba3f8c4.chunk.js"
+  },
+  {
+    "revision": "5a37a8d364e1b484a5fd",
+    "url": "/react-import-export-excel/static/js/runtime~main.66afcff7.js"
+  }
+]);
